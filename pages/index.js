c4d8311633e8848824c419ac20795affd492bc61
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2022-12-29 12:01:07 -0300

    navbar atualizada

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import Nav from '../components/Nav';
+import Navbar from '../components/navbar';
 
 export default function Home() {
   return (
@@ -12,8 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={styles.main}>
-        <Nav />
-        
+        <Navbar />
       </main>
     </>
   )
