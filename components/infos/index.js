7d6f12171ac27fd9e9f0c2db0cb33614195df8ed
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2022-12-29 22:48:49 -0300

    infos

--- components/infos/index.js
+++ components/infos/index.js
@@ -5,9 +5,17 @@ import Image from 'next/image'
 export default function Infos() {
   return (
     <>
-    <Infos>
-      
-    </Infos>
+      <S.Main>
+        <S.Content>
+          <S.Left>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, consectetur.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem ullam, dolorum tempore libero dicta autem?</p>
+          </S.Left>
+          <S.Right>
+            <h1>Imagem</h1>
+          </S.Right>
+        </S.Content>
+      </S.Main>
     </>
   )
 }
--- components/infos/styles.js
+++ components/infos/styles.js
@@ -1,7 +1,32 @@
 import { styled } from '@stitches/react';
 
-export const Infos = styled('div', {
-  width: '100%',
-  height: '100vh',
-  backgroundColor: 'red'
+export const Main = styled('div', {
+  width: '100vw',
+  height: '85vh',
+  backgroundColor: '#919191',
+  display: 'flex',
+  padding: '64px 0px',
 })
+
+export const Content = styled('div', {
+    width: '100%',
+    backgroundColor: '#fff',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+})
+
+export const Left = styled('div', {
+  width: '460px',
+  height: '236px',
+  backgroundColor: 'red',
+  justifyContent: 'center'
+})
+
+export const Right = styled('div', {
+  width: '460px',
+  height: '236px',
+  justifyContent: 'center',
+  textAlign: 'center'
+})
+
