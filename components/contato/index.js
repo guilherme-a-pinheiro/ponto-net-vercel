c3bc82e8798a584c0730ff766e2290d86aefdecc
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-01-09 20:21:04 -0300

    contato pronto

--- components/contato/index.js
+++ components/contato/index.js
@@ -5,19 +5,21 @@ import Image from 'next/image'
 export default function Contato() {
   return (
     <>
-    <S.Main>
+    <S.Contato>
+      <S.Main>
       <S.Content>
+        <S.MainContent>
         <S.Left>
           <S.PontoNet>PONTONET TELECOM</S.PontoNet>
-          <h2>Conectamos Pessoas</h2>
+          <S.h2Left>Conectamos Pessoas</S.h2Left>
           <p>Estamos em busca constante por evolução e aproximar você de quem e o que quiser é nosso principal objetivo. A pontonet tem o compromisso com a qualidade de seus serviços. Referência em fibra óptica, oferece a melhor internet, sem te deixar em apuros.</p>
           <S.Item>
             <S.ContainerItem>
               <S.ImgItem />
             </S.ContainerItem>
               <div>
-                <S.EmailItem>E-mail</S.EmailItem>
-                <div>contato@pontonettelecom.com.br</div>
+                <S.ItemContentDestaque>E-mail</S.ItemContentDestaque>
+                <S.ItemContent>contato@pontonettelecom.com.br</S.ItemContent>
               </div>
           </S.Item>
           <S.Item>
@@ -25,8 +27,8 @@ export default function Contato() {
               <S.ImgItem />
             </S.ContainerItem>
               <div>
-                <S.EmailItem>Telefone</S.EmailItem>
-                <div>44.3013.550</div>
+                <S.ItemContentDestaque>Telefone</S.ItemContentDestaque>
+                <S.ItemContent>44.3013.550</S.ItemContent>
               </div>
           </S.Item>
           <S.Item>
@@ -34,16 +36,19 @@ export default function Contato() {
               <S.ImgItem />
             </S.ContainerItem>
               <div>
-                <S.EmailItem>Endereço</S.EmailItem>
-                <div>Rua Arlindo Marquezini, 358 sala B CEP: 87023-180</div>
+                <S.ItemContentDestaque>Endereço</S.ItemContentDestaque>
+                <S.ItemContent>Rua Arlindo Marquezini, 358 sala B CEP: 87023-180</S.ItemContent>
               </div>
           </S.Item>
         </S.Left>
         <S.Right>
-          <h1>Imagem</h1>
+          <S.ContentRight>
+          </S.ContentRight>
         </S.Right>
-      </S.Content>
-    </S.Main>
+        </S.MainContent>
+        </S.Content>
+      </S.Main>
+    </S.Contato>
     </>
   )
 }
--- components/contato/styles.js
+++ components/contato/styles.js
@@ -1,49 +1,47 @@
 import { styled } from '@stitches/react';
 
+export const Contato = styled('div', {
+})
+
 export const Main = styled('div', {
   width: '100%',
-  height: '100vh',
-  backgroundColor: '#E2F2FF',
   padding: '64px 0px',
-  display: 'flex',
+  backgroundColor: '#e2f2ff',
+  color: 'rgba(0, 0, 0, 0.87)'
 })
 
 export const Content = styled('div', {
-  width: '100%',
-  height: '100%',
-  margin: '0px 60px',
-  backgroundColor: '#E2F2FF',
+  width: '1200px',
+  margin: '0px auto'
+})
+
+export const MainContent = styled('div', {
   display: 'flex',
   justifyContent: 'space-between'
 })
 
 export const Left = styled('div', {
-  width: '435px',
-  height: '100%',
-  backgroundColor: '#E2F2FF',
-  h2: {
-    fontSize: '40px',
-    margin: '16px 0px',
-    padding: '0'
-  }
+  width: '460px',
 })
 
-export const Right = styled('div', {
-  width: '460px',
-  height: '100%',
-  backgroundColor: 'red',
-  borderRadius: '4px',
-  display: 'flex',
-  justifyContent: 'center'
+export const PontoNet = styled('div', {
+  fontSize: '20px',
+  fontWeight: '600',
+  color: '#004682'
 })
+
+export const h2Left = styled('h2', {
+  fontSize: '40px',
+  margin: '16px 0px',
+  padding: '0px'
+})
+
 
 export const Item = styled('div', {
   width: '100%',
-  height: '70px',
-  backgroundColor: '#E2F2FF',
   display: 'flex',
+  padding: '16px 0px',
   borderBottom: '1px solid #CECECE',
-  margin: '10px 0px'
 })
 
 export const ImgItem = styled('div', {
@@ -53,21 +51,34 @@ export const ImgItem = styled('div', {
 })
 
 export const ContainerItem = styled('div', {
-  height: '100%',
-  width: '28px',
-  backgroundColor: '#E2F2FF',
+  height: '28px',
+  borderRadius: '4px',
+  backgroundColor: '#004682',
+  marginRight: '24px',
+  padding: '0px 4px',
   display: 'flex',
-  margin: '0px 24px 0px 0px'
+  alignItems: 'center',
+  justifyContent: 'center'
 })
 
-export const EmailItem = styled('div', {
+export const ItemContentDestaque = styled('div', {
   fontSize: '24px',
   fontWeight: '700',
-  color: '#000'
 })
 
-export const PontoNet = styled('div', {
-  fontSize: '20px',
-  fontWeight: '600',
-  color: '#004682'
+export const ItemContent = styled('div', {
+  fontSize: '16px',
+  fontWeight: '400',
+  marginTop: '4px'
+})
+
+export const Right = styled('div', {
+})
+
+export const ContentRight = styled('div', {
+  width: '534px',
+  height: '534px',
+  backgroundColor: 'rgb(143, 200, 241)',
+  borderRadius: '8px',
+  position: 'relative'
 })
