d497dee3c1223e1d49e704f04dff7f7d74602185
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-01-12 11:46:40 -0300

    qualidades pronto

--- components/qualidades/index.js
+++ components/qualidades/index.js
@@ -11,7 +11,9 @@ export default function Qualidades() {
             <div>
               <S.ItemMain>
                 <S.ItemContent>
-                  <S.ImgContent></S.ImgContent>
+                  <S.ImgContent>
+                    <img src="images/estabilidade_icon_ce8241886d.svg" alt="Estabilidade" />
+                  </S.ImgContent>
                   <h4>Estabilidade</h4>
                   <p>Sem quedas ou atrasos para você assistir seus vídeos sem preocupações</p>
                 </S.ItemContent>
@@ -22,7 +24,9 @@ export default function Qualidades() {
             <div>
               <S.ItemMain>
                 <S.ItemContent>
-                  <S.ImgContent></S.ImgContent>
+                  <S.ImgContent>
+                    <img src="images/disponibilidade_icon_7b10d289ad.svg" alt="Disponibilidade" />
+                  </S.ImgContent>
                   <h4>Disponibilidade</h4>
                   <p>Alta performance para usar o Wi-Fi em seus dispositivos</p>
                 </S.ItemContent>
@@ -33,7 +37,9 @@ export default function Qualidades() {
             <div>
               <S.ItemMain>
                 <S.ItemContent>
-                  <S.ImgContent></S.ImgContent>
+                  <S.ImgContent>
+                    <img src="images/performance_icon_2af8955db0.svg" alt="Performance" />
+                  </S.ImgContent>
                   <h4>Performance</h4>
                   <p>Mantenha seus sistemas funcionando, sem variações</p>
                 </S.ItemContent>
@@ -44,7 +50,9 @@ export default function Qualidades() {
             <div>
               <S.ItemMain>
                 <S.ItemContent>
-                  <S.ImgContent></S.ImgContent>
+                  <S.ImgContent>
+                    <img src="images/qualidade_icon_3bf49e861c.svg" alt="Qualidade" />
+                  </S.ImgContent>
                   <h4>Qualidade</h4>
                   <p>Acesse a internet de vários pontos da sua casa com estabilidade</p>
                 </S.ItemContent>
